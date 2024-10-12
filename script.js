01b7cb0b82f641661d2266d4a37314050ce01d86
Basirat Hamayoon <basirathumayun@gmail.com>
2024-10-12 16:31:36 +0500

    Create function for feed

--- script.js
+++ script.js
@@ -4,5 +4,11 @@ let pet = {
     age: 3,
     happiness: 50,
     hunger: 40,
-  
+    feed: function() {
+        this.hunger -= 20;
+        if (this.hunger < 0) this.hunger = 0;
+        alert(`${this.name} has been fed! Hunger is now at ${this.hunger}.`);
+    },
+   
 }
+
