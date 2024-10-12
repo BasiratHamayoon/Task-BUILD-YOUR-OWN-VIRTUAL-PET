48a74b60796f86a9693005e9248aca562d20dcc4
Basirat Hamayoon <basirathumayun@gmail.com>
2024-10-12 16:34:48 +0500

    create function for age

--- script.js
+++ script.js
@@ -14,6 +14,14 @@ let pet = {
         if (this.happiness > 100) this.happiness = 100;
         alert(`${this.name} played and is now happiness level ${this.happiness}.`);
     },
-    
-}
+    agePet: function() {
+        this.age += 1;
+        this.happiness -= 5;
+        if (this.happiness < 0) this.happiness = 0;
+        this.hunger += 10;
+        if (this.hunger > 100) this.hunger = 100;
+        alert(`${this.name} aged a year! Age: ${this.age}, Happiness: ${this.happiness}, Hunger: ${this.hunger}`);
+    }
+};
+
 
